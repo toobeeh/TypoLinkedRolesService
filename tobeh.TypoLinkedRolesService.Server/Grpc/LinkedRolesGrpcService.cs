8431804fcb3f62418852d595c2614eb7cac7e9fd
tobeh <tobiasch@gmx.at>
2024-08-30 02:08:01 +0200

    fix db concurrency

--- tobeh.TypoLinkedRolesService.Server/Grpc/LinkedRolesGrpcService.cs
+++ tobeh.TypoLinkedRolesService.Server/Grpc/LinkedRolesGrpcService.cs
@@ -1,5 +1,6 @@
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using tobeh.TypoLinkedRolesService.Server.DiscordDtos;
 using tobeh.TypoLinkedRolesService.Server.Service;
 
 namespace tobeh.TypoLinkedRolesService.Server.Grpc;
@@ -13,10 +14,17 @@ public class LinkedRolesGrpcService(
     public override async Task<Empty> UpdateUserMetadata(UpdateUserMetadataMessage request, ServerCallContext context)
     {
         logger.LogTrace("UpdateUserMetadata(request={request})", request);
-        
-        var tasks = request.UserIds.Select(id => Task.Run(async () =>
+
+        var tokensDict = new Dictionary<long, DiscordOauth2TokensDto>();
+        foreach (var id in request.UserIds)
         {
-            var tokens = await discordOauth2Service.GetSavedUserTokens((ulong)id);
+            var tokens = await discordOauth2Service.GetSavedUserTokens((ulong)id); 
+            tokensDict.Add(id, tokens);
+        }
+        
+        var tasks = tokensDict.Keys.Select(id => Task.Run(async () =>
+        {
+            var tokens = tokensDict[id];
             var metadata = await palantirMetatadaService.GetMetadataForMember((ulong)id);
             await discordAppMetadataService.PushUserMetadata(metadata, tokens.AccessToken);
         })).ToArray();
